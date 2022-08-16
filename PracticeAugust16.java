5ab73caba09e9026a5414734206e981c1aa043d7
hyesung.park <48369800+hpark7@users.noreply.github.com>
2022-08-16 08:59:42 -0400

    Create PracticeAugust16.java

---
+++ PracticeAugust16.java
@@ -0,0 +1,15 @@
+public class PracticeAugust16
+{
+  
+  public static void main(String [] args)
+  {
+     System.out.println("Dr. Hyesung Park"); 
+     //Katie Nguyen
+    
+    
+    
+    
+  }
+  
+  
+}
